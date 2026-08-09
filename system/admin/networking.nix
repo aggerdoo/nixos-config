091c6862ff45fc35c9dea96966acd2639d17f65d
aggerdoo <smcklaus@gmail.com>
2026-08-09 17:04:33 +0100

    remove wifi

--- system/admin/networking.nix
+++ system/admin/networking.nix
@@ -2,18 +2,20 @@
 
 {
   networking.networkmanager = {
-    enable = true;
+    enable = lib.mkForce true;
   };
+
+ networking.nameservers = [ "1.1.1.1" "8.8.8.8 "];
 
   networking.wireless = {
-    enable = true;
+    enable = lib.mkForce false;
   };
 
-  networking.wireless.networks = {
-    "ZZOOMM-EBADE5 FAST" = {
-      pskRaw = "Cyhuedvovan6";
-    };
-  };
+  #networking.wireless.networks = {
+  #  "ZZOOMM-EBADE5 FAST" = {
+  #    psk = "Cyhuedvovan6";
+  #  };
+  #};
 
   networking.useDHCP = lib.mkDefault true;
 }
